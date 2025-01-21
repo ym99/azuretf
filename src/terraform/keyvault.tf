6f3bb1190b8395f0c42de68d68801ea28a2119a4
ym991 <yuriy.myshkevych@gmail.com>
2025-01-21 17:00:44 -0500

    initial commit

---
+++ src/terraform/keyvault.tf
@@ -0,0 +1,22 @@
+resource "azurerm_key_vault" "main" {
+  name = "kv-${var.application_name}-${var.enviroment_name}"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "terraform-principal" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_user_assigned_identity.functions.principal_id
+
+  key_permissions = [
+    "Get"
+  ]
+
+}
---
+++ src/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "application_name" {
+    type = string
+}
+
+variable "enviroment_name" {
+    type = string
+}
+
+variable "location" {
+    type = string
+}
