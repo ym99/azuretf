b80c543acbdbae0c12e2f04446bda50851629789
ym991 <yuriy.myshkevych@gmail.com>
2025-01-21 17:47:40 -0500

    fix

--- src/terraform/keyvault.tf
+++ src/terraform/keyvault.tf
@@ -13,7 +13,7 @@ resource "azurerm_key_vault" "main" {
 resource "azurerm_key_vault_access_policy" "terraform-user" {
   key_vault_id = azurerm_key_vault.main.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+  object_id    = data.azurerm_client_config.current.principal_id
 
   key_permissions = [
     "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
