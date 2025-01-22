28cb07efeb5047f9a0810090efa74387da7a9565
ym991 <yuriy.myshkevych@gmail.com>
2025-01-22 13:36:03 -0500

    add uniqueness

--- src/terraform/fn.tf
+++ src/terraform/fn.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "functions" {
-  name                     = "st${var.application_name}${var.environment_name}"
+  name                     = "st${random_string.name}"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
